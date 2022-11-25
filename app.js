94a2bbe5ac354f6657e6d31ee1073d39ce771a36
Juan Buitrago <juan.camilo@grupomarktel.com>
2022-11-25 16:10:00 -0500

    actualizado puerto 80

--- app.js
+++ app.js
@@ -310,6 +310,6 @@ app.put('/updateStateByRecord', cors(corsOptions), function (req, res) {
     }
 });
 
-app.listen(() => {
-    console.log("El servidor está inicializado en el puerto 3000");
+app.listen(80,() => {
+    console.log("El servidor está inicializado en el puerto 80");
 });
