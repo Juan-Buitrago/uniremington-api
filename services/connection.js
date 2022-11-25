0fb7c8644f1278f791ae86006d3a2bf0453a1ab4
Juan Buitrago <juan.camilo@grupomarktel.com>
2022-11-25 15:16:51 -0500

    actualizadas las credenciales de acceso al servidor de mysql

--- services/connection.js
+++ services/connection.js
@@ -6,9 +6,9 @@ connection.getConnection = () =>{
     return new Promise(function (resolve, reject) {
         console.log('mysql connection request');
         let connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'q5f10csw15605',
+            host: 'db-operation-mia-mysql-8.cluster-ccf4ypftnbam.eu-central-1.rds.amazonaws.com',
+            user: 'uniremington',
+            password: 'WIlSrbYU6Vx9',
             database: 'uniremington'
         });
 
